cfa9015cb5b486e1be29fe7101a8746b32f93c96
marciomartinho <marciomartinho@gmail.com>
2019-04-28 14:19:38 -0300

    1 file modified

--- index.js
+++ index.js
@@ -46,3 +46,55 @@ function soma2 (a, b, retorno){
 console.log(soma2(1,2, function(total){
   return total + 10;
 }))
+
+//objeto
+//como se fosse variaveis dentro variavel
+var objeto = {
+  nome: 'a',
+  idade: 5
+}
+console.log(objeto);
+console.log(objeto.idade);//ponto acessa variaveis
+
+objeto.ano = 2019; //consegue add no objeto
+console.log(objeto);
+
+delete objeto.ano;//deleta
+console.log(objeto);
+
+//objeto com funcoes
+var objeto2 = {
+  funcao: function(nome){
+    return `Olá ${nome}`
+  }
+}
+
+console.log(objeto2.funcao('marcio'));
+//outra forma
+var objeto2 = {
+  funcao(nome){
+    return `Olá ${nome}`
+  }
+}
+
+console.log(objeto2.funcao('marcio'));
+
+//juntar objetos
+var teste1 = {
+  nome: "a",
+  idade: 16
+}
+
+var teste2 = {
+  escola: 'estadual',
+  turma: 'b'
+}
+
+var novo = Object.assign({},teste1,teste2);
+console.log(novo);
+//outra forma até mais simples
+var novo2 = {
+  ...teste1,
+  ...teste2
+}
+console.log(novo2);
