2a621ab6c3cb5e3e00a3c65ca0bd664679e674be
marciomartinho <marciomartinho@gmail.com>
2019-04-29 17:43:31 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 //funções
 
-function escreva(){
+function escreva() {
   console.log('olá');
 }
 
 escreva();//função foi escrita acima e foi chamada aqui
 
-function escreva2(mensagem){
+function escreva2(mensagem) {
   console.log(mensagem)
 }
 
 escreva2('teste');
 escreva2('outro teste');//definiu a função com parametros e foi chamando e mudando os parametros
 
-function soma(a, b){
+function soma(a, b) {
   return a + b;
 }
 
-var resultado = soma (19, 1);
+var resultado = soma(19, 1);
 console.log(resultado);
-var resultado2 = soma (10, 1);
+var resultado2 = soma(10, 1);
 console.log(resultado2);
 
-function somaTudo(numeros){
+function somaTudo(numeros) {
   var total = 0;
-  for (var num of numeros){
+  for (var num of numeros) {
     total = total + num;
   }
   return total;
 }
-console.log(numeros[1,2,3]);
+console.log(numeros[1, 2, 3]);
 
-function endereco(rua, cidade, estado, ...complementos){
+function endereco(rua, cidade, estado, ...complementos) {
   console.log(rua);
   console.log(cidade);
   console.log(estado);
@@ -40,10 +40,10 @@ function endereco(rua, cidade, estado, ...complementos){
 endereco('rua A', 'Bsb', 'DF', "apto 1", 'cep 345')
 //complementos com "..." recebe um array
 
-function soma2 (a, b, retorno){
-  return retorno(a+b);
+function soma2(a, b, retorno) {
+  return retorno(a + b);
 }
-console.log(soma2(1,2, function(total){
+console.log(soma2(1, 2, function (total) {
   return total + 10;
 }))
 
@@ -64,7 +64,7 @@ console.log(objeto);
 
 //objeto com funcoes
 var objeto2 = {
-  funcao: function(nome){
+  funcao: function (nome) {
     return `Olá ${nome}`
   }
 }
@@ -72,7 +72,7 @@ var objeto2 = {
 console.log(objeto2.funcao('marcio'));
 //outra forma
 var objeto2 = {
-  funcao(nome){
+  funcao(nome) {
     return `Olá ${nome}`
   }
 }
@@ -90,7 +90,7 @@ var teste2 = {
   turma: 'b'
 }
 
-var novo = Object.assign({},teste1,teste2);
+var novo = Object.assign({}, teste1, teste2);
 console.log(novo);
 //outra forma até mais simples
 var novo2 = {
@@ -98,3 +98,31 @@ var novo2 = {
   ...teste2
 }
 console.log(novo2);
+
+//novo jeito pegar variaveis JS
+var novoUsuario = {
+  nome: 'marcio',
+  idade: 39,
+  pais: 'Brasil'
+}
+var { nome } = novoUsuario; //colocar entre chaves
+console.log(nome);
+console.log(novoUsuario);
+var { nome: primeironome } = novoUsuario; //trocar o nome da variável colocar com :
+console.log(primeironome);
+
+//também vale para objetos dentro de objetos
+var novoUsuario2 = {
+  nome: {
+    primeiro: 'marcio',
+    ultimo: 'martinho',
+    meio: 'rezende'
+  }
+}
+console.log(novoUsuario2);
+console.log(novoUsuario2.nome);
+console.log(novoUsuario2.nome.primeiro);
+var { nome: { primeiro } } = novoUsuario2;//aqui extrai a variável primeiro de dentro do objeto do objeto
+console.log(nome);
+var { nome: { primeiro: mudeiNome } } = novoUsuario2;//mesma lógica para mudar nome variavel
+console.log(mudeiNome);
